e5ab62b90c01458d197796efd07968db954ed9dc
Daniel <danyedo@gmail.com>
2025-07-14 18:44:29 -0400

    CREATE project & first components

---
+++ src/pages/Home.jsx
@@ -0,0 +1,12 @@
+import Container from '../components/Container'
+import Text from '../components/Text'
+
+function Home() {
+    return (
+        <Container as="main">
+            <Text as="h2">Productos</Text>
+        </Container>
+    )
+}
+
+export default Home
