f63af8da59cd2ace816b3ac110c73f1f69adb498
Daniel <danyedo@gmail.com>
2025-07-24 21:49:59 -0400

    ADD Contact US and logic

--- src/pages/ContactUs.jsx
+++ src/pages/ContactUs.jsx
@@ -1,13 +1,84 @@
-import React from 'react'
 import Container from '../components/Container'
+import Form from '../components/Form'
 import Text from '../components/Text'
+import { useForm } from "../hooks/useForm"
+import { postContact } from '../utils/api'
+
+const inputsValidations = {
+    name: {
+     validation: value => value.length > 2,
+     errorText: "El nombre es incorrecto"
+  },
+    email: {
+      validation: value => {
+        const regexp = new RegExp(/^[\w-\.]+@([\w-]+\.)+[\w-]{2,4}$/)
+        return regexp.test (value);
+    }, 
+    errorText: "El email no tiene el formato correcto"
+  },
+    subject: {
+        validation: value => value.length > 0,
+        errorText: "El asunto es obligatorio"
+  },
+    message: {
+        validation: value => value.length > 0,
+          errorText: "Ingrese un texto"
+    }
+  }
 
 function ContactUs() {
-  return (
-        <Container as="main">
-            <Text as="h2">Contactanos</Text>
-        </Container>
-      )
-}
+
+    const {values, onChange, errors} = useForm ({
+        name: "",
+        email: "",
+        subject: "",
+        message: ""
+    }, inputsValidations)
+
+    const handleSubmit = e => {
+        e.preventDefault ()
+        if (Object.values(errors).every( val => !val )) {
+          postContact(values)
+            .then(() => alert("mensaje enviado correctamente"))
+            .catch( err => console.error(err) )
+        } else {
+          console.log("Formulario Invalido")
+        }
+    }
+
+    return (
+          <Container as="main">
+              <Text as="h2">Contactanos</Text>
+              <Form 
+                  values={values}
+                  errors={errors}
+                  onChange={onChange}
+                  onSubmit={handleSubmit}
+                  inputsArray={[
+                    {
+                      name: "name",
+                      type: "text",
+                      label: "Nombre"
+                    },
+                    {
+                      name: "email",
+                      type: "email",
+                      label: "e-Mail"
+                    },
+                    {
+                      name: "subject",
+                      type: "text",
+                      label: "Asunto"
+                    },
+                    {
+                      name: "message",
+                      type: "text",
+                      label: "Mensaje"
+                    }
+                  ]}
+              />
+          </Container>
+        )
+  }
 
 export default ContactUs
--- src/utils/api.js
+++ src/utils/api.js
@@ -1,4 +1,4 @@
-import axios from "axios";
+import axios from "axios"
 
 const axiosInstance = axios.create({
     baseURL: import.meta.env.VITE_BASE_URL
@@ -6,5 +6,10 @@ const axiosInstance = axios.create({
 
 export const getProducts = async () => {
     const resp = await axiosInstance.get("/market-products")
+    return resp.data
+}
+
+export const postContact = async body => {
+    const resp = await axiosInstance.post("/contacts", body)
     return resp.data
 }
